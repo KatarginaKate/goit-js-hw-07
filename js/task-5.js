84b1851865628ed59aa44968c9c5ef9f646509dc
Katargina <katrin-kate@ukr.net>
2026-02-15 14:50:38 +0200

    all tasks

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// Напиши скрипт, який змінює колір фону елемента <body> 
+// через інлайн-стиль по кліку на button.change-color 
+// і задає це значення кольору текстовим вмістом для span.color.
+
+const changeColorBtn = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+});
